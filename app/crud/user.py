2dfb248b957c4c7cedc6b2a2e6e54262364d497c
ProtocolR2 <protocolr2@gmail.com>
2025-07-08 21:36:56 +0200

    Update user.py

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,13 +1,27 @@
 from sqlalchemy.orm import Session
-from app.models.user import User
-from app.schemas.user import UserCreate
+from app import models, schemas
+from sqlalchemy.exc import SQLAlchemyError
 
-def get_user(db: Session, user_id: int):
-    return db.query(User).filter(User.id == user_id).first()
 
-def create_user(db: Session, user: UserCreate):
-    db_user = User(**user.dict())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    return db_user
+def get_user_by_telegram_id(db: Session, telegram_id: int):
+    return db.query(models.User).filter(models.User.telegram_id == telegram_id).first()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = models.User(
+        telegram_id=user.telegram_id,
+        first_name=user.first_name,
+        username=user.username,
+        language_code=user.language_code,
+        email=user.email,
+        plan=user.plan,
+        is_verified=user.is_verified,
+    )
+    try:
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except SQLAlchemyError as e:
+        db.rollback()
+        raise e
