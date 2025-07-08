03cadfa3bb5307ea35aee2542a95237bf4e242fc
ProtocolR2 <protocolr2@gmail.com>
2025-07-08 21:53:12 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,5 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:pass@localhost/dbname")
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://r2:H425iN91es9wnXG0n787h886CsjMm5gr@dpg-d1mlj0u3jp1c73dqtn7g-a.oregon-postgres.render.com/dbr2"
+)
 DEFAULT_LANGUAGE = "es"
