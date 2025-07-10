3f0ce4aec58ef24e93a11dbc5f1129468219e6f1
ProtocolR2 <protocolr2@gmail.com>
2025-07-10 18:24:48 +0200

    Update user.py

--- app/crud/user.py
+++ app/crud/user.py
@@ -1,11 +1,12 @@
+import logging
 from sqlalchemy.orm import Session
 from app import models, schemas
 from sqlalchemy.exc import SQLAlchemyError
 
+logger = logging.getLogger(__name__)
 
 def get_user_by_telegram_id(db: Session, telegram_id: int):
     return db.query(models.User).filter(models.User.telegram_id == telegram_id).first()
-
 
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(
@@ -24,4 +25,5 @@ def create_user(db: Session, user: schemas.UserCreate):
         return db_user
     except SQLAlchemyError as e:
         db.rollback()
-        raise e
+        logger.error(f"Error creando usuario en DB: {e}")
+        raise
