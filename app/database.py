bafa100b542eb52084be155afc221155cb5c4b65
ProtocolR2 <protocolr2@gmail.com>
2025-07-10 18:22:07 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -10,3 +10,11 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base para modelos
 Base = declarative_base()
+
+# Función para obtener la sesión de base de datos y usar con Depends en FastAPI
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
