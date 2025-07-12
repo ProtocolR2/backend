35e3008ae7b48b0aa39aa8c860b935f0182ade1b
ProtocolR2 <protocolr2@gmail.com>
2025-07-12 10:01:06 +0200

    Update create_tables.py

--- app/scripts/create_tables.py
+++ app/scripts/create_tables.py
@@ -1,5 +1,5 @@
 from app.database import Base, engine
-from app.models import receta, mensaje  # importa los modelos para registrarlos
+from app.models import receta, mensaje, plan  # importa los modelos para registrarlos
 
 print("📦 Creando tablas en la base de datos...")
 Base.metadata.create_all(bind=engine)
