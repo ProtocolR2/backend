66731990c15174708cc3ab82e6c3fd45533fef40
ProtocolR2 <protocolr2@gmail.com>
2025-07-12 12:02:06 +0200

    Update admin.py

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -3,16 +3,21 @@ from sqlalchemy.orm import Session
 from starlette.status import HTTP_403_FORBIDDEN
 
 from app.database import get_db, Base, engine
-from app.services.import_recipes import importar_recetas
-from app.models import receta, mensaje  # asegurate de importar todos los modelos que querés crear
+from app.models import receta, mensaje, plan
+from app.services.import_data_from_sheets import importar_todo_desde_sheets
 
 import os
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
 @router.post("/importar-recetas")
-def importar(db: Session = Depends(get_db)):
-    return importar_recetas(db)
+def importar_recetas(request: Request, db: Session = Depends(get_db)):
+    secret = request.headers.get("x-init-secret")
+    if secret != os.getenv("INIT_SECRET"):
+        raise HTTPException(status_code=HTTP_403_FORBIDDEN, detail="No autorizado")
+
+    importar_todo_desde_sheets(db)
+    return {"status": "✅ Datos importados correctamente desde Google Sheets"}
 
 @router.post("/init-db")
 async def init_db(request: Request):
