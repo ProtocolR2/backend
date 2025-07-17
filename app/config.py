1b01dd6a90581b5b0038d5ca2df9ac845ead9f7c
ProtocolR2 <protocolr2@gmail.com>
2025-07-17 10:42:55 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,4 +7,8 @@ DATABASE_URL = os.getenv(
     "DATABASE_URL",
     "postgresql://r2:H425iN91es9wnXG0n787h886CsjMm5gr@dpg-d1mlj0u3jp1c73dqtn7g-a.oregon-postgres.render.com/dbr2"
 )
+
 DEFAULT_LANGUAGE = "es"
+
+# 🚀 Agregamos soporte para múltiples idiomas
+IDIOMAS_SOPORTADOS = ["es", "en", "fr", "ru", "br"]  # Podés agregar más como "pt", "de", etc.
