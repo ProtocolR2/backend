4c3b72be51e838502e268d0cc1dde35abb6cc3b9
ProtocolR2 <protocolr2@gmail.com>
2025-07-18 17:07:19 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.routes import user
 from app.routes import admin  # 👈 NUEVO: importamos las rutas admin
+from app.routes import visualizacion # ver las tablas en postman
+
+app.include_router(visualizacion.router)
 
 app = FastAPI(title="Protocolo R2 Backend")
 
