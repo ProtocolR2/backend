ae5a4fbeba6499e46cd07cb74c3aceb81643250a
ProtocolR2 <protocolr2@gmail.com>
2025-07-19 18:24:01 +0200

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean, BigInteger, Time, DateTime
 from app.database import Base
+import datetime
 
 class User(Base):
     __tablename__ = "usuarios"
@@ -16,4 +17,7 @@ class User(Base):
     telegram_id = Column(BigInteger, unique=True, nullable=True)
     token = Column(String, unique=True, nullable=True)
     fecha_activacion = Column(DateTime, nullable=True)
-    horario_envio = Column(Time, default="08:00", nullable=True)
+    horario_envio = Column(Time, default=datetime.time(8, 0), nullable=True)
+
+    def __repr__(self):
+        return f"<User id={self.id} telegram_id={self.telegram_id} username={self.username}>"
