80ea77814e1ee94ec9cb19d6f11384e4264cfc8f
ProtocolR2 <protocolr2@gmail.com>
2025-07-19 18:34:45 +0200

    Update user.py

--- app/crud/user.py
+++ app/crud/user.py
@@ -2,6 +2,7 @@ import logging
 from sqlalchemy.orm import Session
 from app import models, schemas
 from sqlalchemy.exc import SQLAlchemyError
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +18,9 @@ def create_user(db: Session, user: schemas.UserCreate):
         email=user.email,
         plan=user.plan,
         is_verified=user.is_verified,
+        token=user.token,
+        fecha_activacion=user.fecha_activacion,
+        horario_envio=user.horario_envio,
     )
     try:
         db.add(db_user)
@@ -26,4 +30,18 @@ def create_user(db: Session, user: schemas.UserCreate):
     except SQLAlchemyError as e:
         db.rollback()
         logger.error(f"Error creando usuario en DB: {e}")
+        raise
+
+def update_user(db: Session, db_user: models.User, user_update: schemas.UserUpdate):
+    if user_update.first_name is not None:
+        db_user.first_name = user_update.first_name
+    if user_update.horario_envio is not None:
+        db_user.horario_envio = user_update.horario_envio
+    try:
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except SQLAlchemyError as e:
+        db.rollback()
+        logger.error(f"Error actualizando usuario en DB: {e}")
         raise
