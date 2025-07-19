a826843745a3fa890b0d9f4f349c37dd804a2f94
ProtocolR2 <protocolr2@gmail.com>
2025-07-19 17:51:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
 from app.config import DEFAULT_LANGUAGE
 from app.models import Base
 from app.database import engine
+
+# 👇 importar el router principal
+from app.api.api import api_router
 
 app = FastAPI(
     title="ProtocolR2 Backend",
@@ -13,6 +17,9 @@ app = FastAPI(
 # Crear las tablas automáticamente en PostgreSQL
 Base.metadata.create_all(bind=engine)
 
+# Agregar los routers externos
+app.include_router(api_router)
+
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
@@ -22,7 +29,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Ruta base
+# Rutas base
 @app.get("/")
 def read_root():
     return {"status": "ok", "lang_default": DEFAULT_LANGUAGE}
