6bfad95c11e01f31d3d9eca033a607037abeac52
ProtocolR2 <protocolr2@gmail.com>
2025-07-19 18:30:25 +0200

    Update user.py

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from typing import Optional
-from datetime import datetime
+from datetime import datetime, time
 
 class UserBase(BaseModel):
     telegram_id: int
@@ -10,9 +10,16 @@ class UserBase(BaseModel):
     email: Optional[EmailStr] = None
     plan: Optional[str] = "free"
     is_verified: Optional[bool] = False
+    token: Optional[str] = None
+    fecha_activacion: Optional[datetime] = None
+    horario_envio: Optional[time] = None
 
 class UserCreate(UserBase):
     pass
+
+class UserUpdate(BaseModel):
+    first_name: Optional[str] = None
+    horario_envio: Optional[time] = None
 
 class UserInDB(UserBase):
     id: int
