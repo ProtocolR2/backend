aeff9e659a5e727349509a20a4d0d051634f1035
ProtocolR2 <protocolr2@gmail.com>
2025-07-24 17:50:17 +0200

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, BigInteger, Time, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, BigInteger, Time, DateTime, ARRAY
 from app.database import Base
 import datetime
 
@@ -13,11 +13,14 @@ class User(Base):
     plan = Column(String, default="free")
     is_verified = Column(Boolean, default=False)
 
-    # Nuevos campos para onboarding y seguimiento
     telegram_id = Column(BigInteger, unique=True, nullable=True)
     token = Column(String, unique=True, nullable=True)
     fecha_activacion = Column(DateTime, nullable=True)
     horario_envio = Column(Time, default=datetime.time(8, 0), nullable=True)
+
+    # 🆕 Nuevos campos
+    cliente_id = Column(String, unique=True, nullable=True, index=True)
+    programas_activos = Column(ARRAY(String), default=["R2"])
 
     def __repr__(self):
         return f"<User id={self.id} telegram_id={self.telegram_id} username={self.username}>"
